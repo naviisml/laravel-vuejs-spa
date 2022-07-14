54915d608922ce2931411f00aa66d363040a3c62
Navi <mailvoornavi@gmail.com>
2022-07-14 02:55:40 +0200

    refactor `OAuthController` with various bugfixes

--- app/Http/Controllers/Auth/OAuthController.php
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -2,19 +2,29 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Exceptions\EmailTakenException;
-use App\Http\Controllers\Controller;
-use App\Models\OAuthProvider;
-use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Contracts\User as SocialiteUser;
 use Laravel\Socialite\Facades\Socialite;
+use App\Exceptions\EmailTakenException;
+use App\Http\Controllers\Controller;
+use App\Models\OAuthProvider;
+use App\Models\User;
 
 class OAuthController extends Controller
 {
     use AuthenticatesUsers;
+
+    /**
+     * @var array
+     */
+    protected $provider = null;
+
+    /**
+     * @var string
+     */
+    protected $driver = null;
 
     /**
      * Create a new controller instance.
@@ -25,22 +35,40 @@ class OAuthController extends Controller
     }
 
     /**
-     * Redirect the user to the provider authentication page.
+     * Retrieve and return the redirect_url for a specific driver
+     *
+     * @param   string  $driver
+     *
+     * @return  \Illuminate\Support\Facades\Response
      */
-    public function redirect(string $provider)
+    public function redirect(string $driver = null)
     {
-        $url = Socialite::driver($provider)->stateless()->redirect()->getTargetUrl();
+        // set the provider
+        if (!is_null($driver)) {
+            $this->setDriver($driver);
+        }
+
+        $url = Socialite::driver($this->driver)->stateless()->redirect()->getTargetUrl();
 
         return response()->json(['url' => $url]);
     }
 
     /**
-     * Obtain the user information from the provider.
+     * The authentication callback
+     *
+     * @param   string  $driver
+     *
+     * @return  \Illuminate\Support\Facades\View
      */
-    public function handleCallback(string $provider)
+    public function handleCallback(string $driver)
     {
-        $user = Socialite::driver($provider)->stateless()->user();
-        $user = $this->findOrCreateUser($provider, $user);
+        // set the driver
+        if (!is_null($driver)) {
+            $this->setDriver($driver);
+        }
+
+        $user = Socialite::driver($this->driver)->stateless()->user();
+        $user = $this->findOrCreateUser($user);
 
         $this->guard()->setToken(
             $token = $this->guard()->login($user)
@@ -57,75 +85,132 @@ class OAuthController extends Controller
     }
 
     /**
-     * Find or create a user.
+     * Find, create or link a user
+     *
+     * @param   SocialiteUser  $socialite
+     *
+     * @return  User
      */
-    protected function findOrCreateUser(string $provider, SocialiteUser $socialite): User
+    protected function findOrCreateUser(SocialiteUser $socialite): User
     {
-        $oauthProvider = OAuthProvider::where('provider', $provider)
+        $this->provider = OAuthProvider::where('provider', $this->driver)
             ->where('provider_user_id', $socialite->getId())
             ->first();
 
-        if ($oauthProvider) {
-            $user = $oauthProvider->user;
-
-            $oauthProvider->update([
-                'provider_user_data' => json_encode($socialite, true),
-                'access_token' => $socialite->token,
-                'refresh_token' => $socialite->refreshToken,
-            ]);
-
-            // Log the action
-            $user->log('user.login', ['provider' => $provider]);
-
-            return $user;
+        // check if the provider is not linked, and a user is logged in
+        if (($user = Auth::guard('web')->user()) && !$this->provider) {
+            return $this->linkUser($socialite, $user);
         }
 
-        return $this->createUser($provider, $socialite);
+        // check if the account linked to another account
+        if ($user && $this->provider && $this->provider->user_id !== $user->id) {
+            return abort(403);
+        }
+
+        // check if the provider already exists
+        if ($this->provider && ($user = $this->provider->user)) {
+            return $this->updateUser($socialite, $user);
+        }
+
+        // create
+        return $this->createUser($socialite);
     }
 
     /**
-     * Create a new user.
+     * Create a new user
+     *
+     * @param   SocialiteUser  $socialite
+     *
+     * @return  User
      */
-    protected function createUser(string $provider, SocialiteUser $socialite): User
+    protected function createUser(SocialiteUser $socialite): User
     {
-        // check if the user exists, to link the account to that user, or
-        // create a new user.
-        // NOTE: this is done through cookies, which kinda defeats the purpose
-        // of the api, but this is only used as a *side* service (I need to change this..)
-        if (!($user = Auth::guard('web')->user())) {
-            if (User::where('email', $socialite->getEmail())->exists()) {
-                throw new EmailTakenException;
-            }
-
-            $user = User::create([
-                'username' => $socialite->getName(),
-                'email' => $socialite->getEmail()
-            ]);
-
-            // Assign the default role to the user
-            $user->roles()->create([
-                'user_id' => $user->id,
-                'role' => config('roles.default.tag')
-            ]);
-
-            // Log the action
-            $user->log('user.create', ['provider' => $provider]);
-        } else {
-            // Log the action
-            $user->log('user.link', ['provider' => $provider]);
+        if (User::where('email', $socialite->getEmail())->exists()) {
+            throw new EmailTakenException;
         }
 
+        $user = User::create([
+            'username' => $socialite->getNickname(),
+            'email' => $socialite->getEmail() ?? ''
+        ]);
+
+        // Assign the default role to the user
+        $user->roles()->create([
+            'user_id' => $user->id,
+            'role' => config('roles.default.tag')
+        ]);
+
+        // Log the action
+        $user->log('user.create', ['provider' => $this->driver]);
+
 		// Create the OAuth provider entry
-		$user->oauthProviders()->create([
-			'provider' => $provider,
+		$user = $this->linkUser($socialite, $user);
+
+        // verify the email
+		$user->markEmailAsVerified();
+
+        return $user;
+    }
+
+    /**
+     * Update a existing user
+     *
+     * @param   SocialiteUser  $socialite
+     * @param   User           $user
+     *
+     * @return  User
+     */
+    protected function updateUser(SocialiteUser $socialite, User $user): User
+    {
+        $this->provider->update([
+            'provider_user_data' => json_encode($socialite, true),
+            'access_token' => $socialite->token,
+            'refresh_token' => $socialite->refreshToken,
+        ]);
+
+        // Log the action
+        $user->log('user.login', ['provider' => $this->driver]);
+
+        return $user;
+    }
+
+    /**
+     * Link a driver to a existing user
+     *
+     * @param   SocialiteUser  $socialite
+     * @param   User           $user
+     *
+     * @return  User
+     */
+    protected function linkUser(SocialiteUser $socialite, User $user): User
+    {
+		$this->provider = $user->oauthProviders()->create([
+			'provider' => $this->driver,
 			'provider_user_id' => $socialite->getId(),
             'provider_user_data' => json_encode($socialite, true),
 			'access_token' => $socialite->token,
 			'refresh_token' => $socialite->refreshToken,
 		]);
 
-		$user->markEmailAsVerified();
+        // Log the action
+        $user->log('user.link', ['provider' => $this->driver]);
 
         return $user;
+    }
+
+    /**
+     * Set the driver
+     *
+     * @param   string  $driver
+     *
+     * @return  $this->driver
+     */
+    protected function setDriver($driver = null)
+    {
+        if (!is_null($driver)) {
+            $this->driver = $driver;
+        }
+
+        return $this->driver;
     }
 }
